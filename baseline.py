b3b9375592002c0c8d1ee866f86b03eac7f351e9
Claire Huang <chuang20@stanford.edu>
2018-05-27 09:25:12 -0700

    Smarter Q learning, added evolutionary

--- baseline.py
+++ baseline.py
@@ -5,7 +5,7 @@ import time
 import numpy as np
 import random
 
-from policy import RandomPolicy, QLearning_NaiveDiscretize
+from policy import RandomPolicy, QLearning_NaiveDiscretize, QLearning_SmartDiscretize
 
 import matplotlib
 # matplotlib.use('TkAgg')
@@ -31,28 +31,25 @@ env = gym.make('BipedalWalker-v2')
 # print(env.observation_space.high)
 # print(env.observation_space.low)
 
-# # Discretize action space
-# action_space_low = env.action_space.low[0]
-# action_space_high = env.action_space.high[0]
-# action_space_num = 5
-# space = np.linspace(action_space_low, action_space_high, action_space_num, endpoint=True)
-# action_space = [ (a,b,c,d) for a in space for b in space for c in space for d in space ]
-
-
+WRITE_TO_FILE = False
 
 # policy = RandomPolicy(env)
-policy = QLearning_NaiveDiscretize(env)
+# policy = QLearning_NaiveDiscretize(env)
+policy = QLearning_SmartDiscretize(env)
 
-output_file = open("results/{}_{}.csv".format(policy.get_name(), int(time.time())), "w+")
-output_file.write(('{},{},{}\n').format('num_timesteps', 'total_reward', 'fell'))
+if WRITE_TO_FILE:
+	output_file = open("results/{}_{}.csv".format(policy.get_name(), int(time.time())), "w+")
+	output_file.write(('{},{},{}\n').format('num_timesteps', 'total_reward', 'fell'))
 
 #############################################
 #		 	   Execute episodes 	 	    #
 #############################################
 rewards = []
+avg_rewards = []
 timesteps = []
-num_episodes = 300
+num_episodes = 500
 times_fallen = 0
+avg_reward = None
 
 for episode in range(num_episodes):
 	state = env.reset()
@@ -63,16 +60,21 @@ for episode in range(num_episodes):
 	if episode % (num_episodes / 5) == 0:
 		print('============ EPISODE {} ============').format(episode)
 
-	while not done and num_timesteps <= 200:
+	while not done: # and num_timesteps <= 500:
+		# env.render()
 		# Render the last episode
-		if (episode == num_episodes - 1):
-			env.render()
+		# if (episode == num_episodes - 1):
+		# 	env.render()
 
 		# Select action
-		action = policy.get_action(state)
+		action = policy.get_action(state, float(episode)/num_episodes)
+		# print action
 
 		# Perform action
 		result_state, reward, done, info = env.step(action)
+		if (reward > 0):
+			reward *= 50
+		# print(reward)
 		policy.update(state, action, reward, result_state)
 
 		state = result_state
@@ -83,24 +85,29 @@ for episode in range(num_episodes):
 		# print('reward', reward)
 		# print('info', info)
 
-	print('num timesteps: {}, reward: {}').format(num_timesteps, total_reward)
+	avg_reward = 0.9 * avg_reward + 0.1 * total_reward if avg_reward != None else total_reward
+	print('num timesteps: {}, reward: {}, avg_reward: {}').format(num_timesteps, total_reward, avg_reward)
 	times_fallen = times_fallen + 1 if done else times_fallen
+	avg_rewards.append(avg_reward)
 	rewards.append(total_reward)
 	timesteps.append(num_timesteps)
-	output_file.write(('{},{},{}\n').format(num_timesteps, total_reward, done))
+	if WRITE_TO_FILE:
+		output_file.write(('{},{},{}\n').format(num_timesteps, total_reward, done))
 
 #############################################
 #		 	  	Report results	 	    	#
 #############################################
-output_file.close()
+if WRITE_TO_FILE:
+	output_file.close()
 
 print('fell {}%').format(float(times_fallen) / num_episodes * 100)
 
 plt.figure(1)
 
 plt.subplot(211)
-plt.plot(rewards)
-plt.title('Q learning')
+plt.plot(rewards, 'b')
+plt.plot(avg_rewards, 'r')
+plt.title(('{}').format(policy.get_name()))
 plt.ylabel('reward')
 
 plt.subplot(212)
--- policy.py
+++ policy.py
@@ -11,7 +11,7 @@ class RLAlgorithm:
 	def __init__(self, env): raise NotImplementedError("Override me")
 
 	# Produce action given state
-	def get_action(self, state): raise NotImplementedError("Override me")
+	def get_action(self, state, episode_percentage): raise NotImplementedError("Override me")
 
 	# Incorporate environment feedback after an action
 	def update(self, state, action, reward, new_state): raise NotImplementedError("Override me")
@@ -28,7 +28,7 @@ class RandomPolicy(RLAlgorithm):
 	def __init__(self, env):
 		self.action_space = env.action_space
 
-	def get_action(self, state):
+	def get_action(self, state, episode_percentage):
 		return self.action_space.sample()
 
 	def update(self, state, action, reward, new_state):
@@ -39,7 +39,7 @@ class RandomPolicy(RLAlgorithm):
 
 
 #############################################
-#		 	    NAIVE Q LEARNING		 	#
+#		 	   NAIVE Q LEARNING			 	#
 #											#
 #  Basic Q learning on naively discretized 	#
 #  			action and state spaces 	 	#
@@ -84,7 +84,7 @@ class QLearning_NaiveDiscretize(RLAlgorithm):
 
 		return (best_action, best_Q)
 
-	def get_action(self, state):
+	def get_action(self, state, episode_percentage):
 		# Sometimes pick random action to explore
 		if np.random.random() > self.exploration_prob:
 			return random.choice(self.action_space)
@@ -102,5 +102,108 @@ class QLearning_NaiveDiscretize(RLAlgorithm):
 		return 'QLearning_NaiveDiscretize'
 
 
+#############################################
+#		      SMARTER? Q LEARNING		 	#
+#											#
+#############################################
+class QLearning_SmartDiscretize(RLAlgorithm):
+	def __init__(self, env):
+		# Discretize action space
+		# action_space_low = env.action_space.low[0]   # -1
+		# action_space_high = env.action_space.high[0] # +1
+		action_space_low = -0.4
+		action_space_high = 0.4
+		action_space_num = 10
+		space = np.linspace(action_space_low, action_space_high, action_space_num, endpoint=True)
+		self.space = space
+		self.action_space = [ (a,b,c,d) for a in space for b in space for c in space for d in space ]
+
+		self.Q = defaultdict(float)
+
+		# Tunable constants
+		self.exploration_prob = .2
+		self.discount_rate = 1
+		self.learning_rate = 1
+
+		self.num_iterations = 0
+
+	# Discretize state space 
+	# 	Skip lidar readings
+	def continuous2discrete_state(self, state):
+		discrete_state = []
+
+		num_buckets = 10
+
+		# Hull angle, hip joint angles (x2), knee joint angles (x2) (0, 2*pi)
+		lo = -np.pi
+		hi = np.pi
+		bucket_size = float(hi-lo) / num_buckets
+		# print state[4]
+		# print (np.clip(state[4], lo, hi) - lo) / bucket_size
+		discrete_state.append( int((np.clip(state[0], lo, hi) - lo) / bucket_size) )
+		discrete_state.append( int((np.clip(state[4], lo, hi) - lo) / bucket_size) )
+		discrete_state.append( int((np.clip(state[6], lo, hi) - lo) / bucket_size) )
+		discrete_state.append( int((np.clip(state[9], lo, hi) - lo) / bucket_size) )
+		discrete_state.append( int((np.clip(state[11], lo, hi) - lo) / bucket_size) )
+
+		# # Velocity x, y (-1, 1)
+		# lo = -1
+		# hi = 1
+		# bucket_size = float(hi-lo) / num_buckets
+		# discrete_state.append( int((state[2] - lo) / bucket_size) )
+		# discrete_state.append( int((state[3] - lo) / bucket_size) )
+
+		# # Hip joint speeds (x2), knee joint speeds (x2)
+		# lo = -2
+		# hi = 2
+		# bucket_size = float(hi-lo) / num_buckets
+		# discrete_state.append( int((np.clip(state[5], lo, hi) - lo) / bucket_size) )
+		# discrete_state.append( int((np.clip(state[7], lo, hi) - lo) / bucket_size) )
+		# discrete_state.append( int((np.clip(state[10], lo, hi) - lo) / bucket_size) )
+		# discrete_state.append( int((np.clip(state[12], lo, hi) - lo) / bucket_size) )
+
+		# Ground contact flags
+		discrete_state.append( int(state[8]) )
+		discrete_state.append( int(state[13]) )
+
+		# print discrete_state
+		return tuple(discrete_state)
+
+	# Find the next action that maximizes Q given the current state
+	# Return (maximizing action, max Q)
+	def getBestAction_Q(self, state):
+		best_action = None
+		best_Q = -100
+
+		for action in self.action_space:
+			if self.Q[(state, action)] >= best_Q:
+				best_Q = self.Q[(state, action)]
+				best_action = action
+
+		return (best_action, best_Q)
+
+	def get_exploration_prob(self, episode_percentage):
+		return -1 * (episode_percentage - 1) ** 3
+
+	def get_action(self, state, episode_percentage):
+		# Sometimes pick random action to explore
+		if np.random.random() > self.get_exploration_prob(episode_percentage):
+			return (random.choice(self.space), random.choice(self.space), random.choice(self.space), random.choice(self.space))
+		else: # Otherwise find action that maximizes Q
+			state = self.continuous2discrete_state(state)
+			return self.getBestAction_Q(state)[0]
+
+	def update(self, state, action, reward, new_state):
+		state = self.continuous2discrete_state(state)
+		new_state = self.continuous2discrete_state(new_state)
+		_, new_state_value = self.getBestAction_Q(new_state)
+		update = self.learning_rate * (self.Q[(state, action)] - (reward + self.discount_rate * new_state_value))
+		self.Q[(state, action)] -= update
+		# if (np.abs(self.Q[(state, action)]) != np.abs(update)):
+		# 	print('{} {}'.format(update, self.Q[(state, action)]))
+
+	def get_name(self):
+		return 'QLearning_SmartDiscretize'
+		
 
 
